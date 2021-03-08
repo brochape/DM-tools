332819255568fd577b88aab1ab2d2b0534378f88
Bruno ROCHA PEREIRA <b.rocha.per@gmail.com>
2021-03-08 13:22:55 +0100

    Styling

--- dm-tools-ui/src/molecule/RollDicePage/RollDicePage.tsx
+++ dm-tools-ui/src/molecule/RollDicePage/RollDicePage.tsx
@@ -1,29 +1,49 @@
 import * as React from 'react';
 import { FunctionComponent, useEffect, useState } from 'react';
 import { DiceRoll } from 'rpg-dice-roller';
+import './RollDicePage.css';
 
 export interface IAppProps {}
 
 export const RollDicePage: FunctionComponent = () => {
-  let [diceString, setDiceString] = useState<string>('');
-  let [diceRollTotal, setDiceRollTotal] = useState<string>('');
-  useEffect(() => {
-    try {
-      setDiceRollTotal('' + new DiceRoll(diceString).output);
-    } catch (ex) {
-      setDiceRollTotal('Invalid formula');
-    }
-  }, [diceString]);
+    let [diceString, setDiceString] = useState<string>('');
+    let [diceRollTotal, setDiceRollTotal] = useState<string>('');
+    let [diceRollDetail, setDiceRollDetail] = useState<string>('');
+    useEffect(() => {
+        try {
+            const formulaOutput = ('' + new DiceRoll(diceString).output).split(
+                ' = '
+            );
+            setDiceRollDetail(formulaOutput[0].split(': ')[1]);
+            setDiceRollTotal(formulaOutput[1]);
+        } catch (ex) {
+            setDiceRollDetail('Invalid formula');
+            setDiceRollTotal('0');
+        }
+    }, [diceString]);
 
-  const inputChanged = (event) => {
-    console.log(event);
-    setDiceString(event.target.value);
-  };
-  return (
-    <div className={'mainPage'}>
-      <div className={'mainPageTitle'}>Roll dice</div>
-      <input onChange={(evt) => inputChanged(evt)} value={diceString} />
-      <div>{diceRollTotal}</div>
-    </div>
-  );
+    const inputChanged = (event) => {
+        console.log(event);
+        setDiceString(event.target.value);
+    };
+    return (
+        <div className={'mainPage'}>
+            <div className={'mainPageTitle'}>Roll dice</div>
+            <div className={'formulaInputGroup'}>
+                <input
+                    className={'formulaInput'}
+                    id={'formula'}
+                    onChange={(evt) => inputChanged(evt)}
+                    value={diceString}
+                    placeholder="Dice Formula"
+                    required
+                />
+                <label htmlFor={'formula'} className={'formulaInputLabel'}>
+                    Dice formula
+                </label>
+            </div>
+            <div className="formulaResultDetail">{diceRollDetail}</div>
+            <div className="formulaResultTotal">{diceRollTotal}</div>
+        </div>
+    );
 };
